ecf2067c09e19f121287346436e05f42b8b444fd
Pete Turnbull <pete.j.turnbull@gmail.com>
2018-07-03 14:37:57 +0100

    update pkg

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ const hash = require('fnv1a')
 const Hashids = require('hashids')
 const hashids = new Hashids('', 32, 'abcdefghijklmnopqrstuvwxyz0123456789')
 
-const create = (seed) => {
+const create = seed => {
   if (seed && seed !== '') {
     const _id = hashids.encode(hash(seed))
-    return `${_id.slice(0, 8)}-${_id.slice(8, 12)}-${_id.slice(12, 16)}-${_id.slice(16, 20)}-${_id.slice(20)}`
+    return `${_id.slice(0, 8)}-${_id.slice(8, 12)}-${_id.slice(
+      12,
+      16,
+    )}-${_id.slice(16, 20)}-${_id.slice(20)}`
   }
   return uuid.v4()
 }
@@ -14,4 +17,3 @@ const create = (seed) => {
 module.exports = {
   create,
 }
-
